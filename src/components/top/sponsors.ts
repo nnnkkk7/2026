ba7fb0161933ee1868d5aa5d87a9e53d87cd6fdb
masakurapa <rabasan1031@gmail.com>
2026-05-08 21:49:08 +0900

    スポンサーリストのサンプル

--- src/components/top/sponsors.ts
+++ src/components/top/sponsors.ts
@@ -10,23 +10,68 @@ export type Sponsor = {
   };
 };
 
+import img1 from "../../assets/footer_logo.svg";
+import img2 from "../../assets/page-header-gopher.png";
+
 export const goldSponsors: Sponsor[] = [
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold1", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold2", link:"", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold3", link:"http://localhost:4321", description: ""}},
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold4", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold5", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "gold sponsor",  imageSrc: img1 ,dialog:{openId:"gold6", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
 ];
 
 export const silverSponsors: Sponsor[] = [
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver1", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver2", link:"", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver3", link:"http://localhost:4321", description: ""}},
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver4", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver5", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "silver sponsor",  imageSrc: img1 ,dialog:{openId:"silver6", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
 ];
 
 export const lunchSponsors: Sponsor[] = [
+  { name: "lunch sponsor",  imageSrc: img1 ,dialog:{openId:"lunch1", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "lunch sponsor",  imageSrc: img1 ,dialog:{openId:"lunch2", link:"", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+
 ];
 
 export const drinkSponsors: Sponsor[] = [
+  { name: "drink sponsor",  imageSrc: img1 ,dialog:{openId:"drink1", link:"http://localhost:4321", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
+  { name: "drink sponsor",  imageSrc: img1 ,dialog:{openId:"drink2", link:"", description: "dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description. dummy description."}},
 ];
 
 export const bronzeSponsors: Sponsor[] = [
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
+  { name: "bronze sponsor",  imageSrc: img1 },
 ];
 
 export const gopherSponsors: Sponsor[] = [
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
+  { name: "gopher sponsor",  imageSrc: img2 },
 ];
 
 export const toolSponsors: Sponsor[] = [
+  { name: "tool sponsor",  imageSrc: img1 },
 ];
